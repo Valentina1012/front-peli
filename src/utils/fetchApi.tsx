b62df47291e4e566217912ea9ecac74337d46a80
valentinochiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-03-09 19:32:36 -0300

    cambios atras

--- src/utils/fetchApi.tsx
+++ src/utils/fetchApi.tsx
@@ -3,11 +3,6 @@ import axios from "axios";
 const makeApi = () => {
   return axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    headers: { 
-      'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    },
-    withCredentials: true
   });
 };
 
