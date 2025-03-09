da58921e614bdd78091b49e755eefa99c14f4ea5
valentinochiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2025-03-09 19:27:36 -0300

    fix

--- src/utils/fetchApi.tsx
+++ src/utils/fetchApi.tsx
@@ -6,7 +6,8 @@ const makeApi = () => {
     headers: { 
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-    }
+    },
+    withCredentials: true
   });
 };
 
